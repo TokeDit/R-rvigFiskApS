a04d80f4763e97d9458056e79010257cdcae3f5f
TokeDit <tokedit11@gmail.com>
2026-05-26 15:59:25 +0200

    :+)

--- src/pages/Arrangementer.vue
+++ src/pages/Arrangementer.vue
@@ -1,5 +1,12 @@
+<script setup>
+import GiftCard from '@/components/GiftCard.vue';
+</script>
+
 <template>
   <main>
     <h1>Arrangementer / Events</h1>
+     
+
+    <GiftCard />
   </main>
 </template>
--- src/pages/OmOs.vue
+++ src/pages/OmOs.vue
@@ -1,5 +1,9 @@
 <template>
   <main>
     <h1>Om os</h1>
+    <p>
+      Dette er en om os page der forteller om rorvigfisks historie og
+      ejereforhold.
+    </p>
   </main>
 </template>
